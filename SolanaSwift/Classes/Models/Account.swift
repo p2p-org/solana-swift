d96beaa9cb4ee555cfa458c09521d461d4025288
Chung Tran <bigearsenal@gmail.com>
2020-11-06 12:27:34 +0700

    decode Account.Meta

--- SolanaSwift/Classes/Models/Account.swift
+++ SolanaSwift/Classes/Models/Account.swift
@@ -38,5 +38,13 @@ public extension SolanaSDK.Account {
         public let publicKey: SolanaSDK.PublicKey
         public let isSigner: Bool
         public let isWritable: Bool
+    
+        public init(from decoder: Decoder) throws {
+            let value = try decoder.singleValueContainer()
+            let string = try value.decode(String.self)
+            publicKey = try SolanaSDK.PublicKey(string: string)
+            isSigner = false
+            isWritable = false
+        }
     }
 }
