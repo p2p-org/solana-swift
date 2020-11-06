0a6413c0529b4270d1b8fde51ee5b869558d100f
Chung Tran <bigearsenal@gmail.com>
2020-11-06 15:50:31 +0700

    some fixes

--- SolanaSwift/Classes/Models/SystemProgram.swift
+++ SolanaSwift/Classes/Models/SystemProgram.swift
@@ -12,15 +12,16 @@ public extension SolanaSDK {
         public static let PROGRAM_ID = try! PublicKey(string: "11111111111111111111111111111111")
         private static let PROGRAM_INDEX_TRANSFER = UInt32(2)
         
-        public static func transfer(from fromPublicKey: PublicKey, to toPublicKey: PublicKey, lamports: UInt64) -> Transaction.Instruction {
+        public static func transfer(from fromPublicKey: PublicKey, to toPublicKey: PublicKey, lamports: Int64) -> Transaction.Instruction {
             var keys = [Account.Meta]()
             keys.append(Account.Meta(publicKey: fromPublicKey, isSigner: true, isWritable: true))
             keys.append(Account.Meta(publicKey: toPublicKey, isSigner: false, isWritable: false))
             
             // 4 byte instruction index + 8 bytes lamports
-            var data = Data(capacity: 12)
+            var data = Data()
             data.append(contentsOf: SystemProgram.PROGRAM_INDEX_TRANSFER.bytes)
-            data.append(contentsOf: lamports.bytes)
+            let array = withUnsafeBytes(of: lamports.littleEndian, Array.init)
+            data.append(contentsOf: array)
             return Transaction.Instruction(keys: keys, programId: SystemProgram.PROGRAM_ID, data: data.bytes)
         }
     }
