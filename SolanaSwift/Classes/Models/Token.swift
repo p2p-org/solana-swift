bab840869e129ac89cd7b78dfc44577e9f239464
Chung Tran <bigearsenal@gmail.com>
2020-11-17 15:11:33 +0700

    add owner

--- SolanaSwift/Classes/Models/Token.swift
+++ SolanaSwift/Classes/Models/Token.swift
@@ -11,6 +11,7 @@ public extension SolanaSDK {
     struct Token: Decodable, Hashable {
         public let name: String
         public let mintAddress: String
+        public var owner: String?
         public let symbol: String
         public let icon: String?
         public var amount: UInt64?
@@ -27,6 +28,7 @@ public extension SolanaSDK {
             if let token = supportedTokens.first(where: {$0.mintAddress == mintAddress}) {
                 self = token
                 self.amount = accountInfo.data.amount
+                self.owner = accountInfo.data.owner?.base58EncodedString
                 return
             }
             
