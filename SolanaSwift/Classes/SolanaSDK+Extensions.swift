d0d9135d4be127d607b90fc0781a179cb4c9b61a
Chung Tran <bigearsenal@gmail.com>
2020-11-23 16:15:07 +0700

    fix send API

--- SolanaSwift/Classes/SolanaSDK+Extensions.swift
+++ SolanaSwift/Classes/SolanaSDK+Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 
 extension SolanaSDK {
-    public func send(to toPublicKey: String, amount: Int64) -> Single<String> {
+    public func send(from fromPublicKey: String, to toPublicKey: String, amount: Int64) -> Single<String> {
         getRecentBlockhash()
             .map {$0.blockhash}
             .map { recentBlockhash -> String in
@@ -22,11 +22,12 @@ extension SolanaSDK {
                 guard let account = self.accountStorage.account else {
                     throw Error.publicKeyNotFound
                 }
+                let fromPublicKey = try PublicKey(string: fromPublicKey)
                 let toPublicKey = try PublicKey(string: toPublicKey)
-                let signer = self.accountStorage.account!
+                let signer = account
                 
                 var transaction = Transaction()
-                transaction.message.add(instruction: SystemProgram.transfer(from: account.publicKey, to: toPublicKey, lamports: amount))
+                transaction.message.add(instruction: SystemProgram.transfer(from: fromPublicKey, to: toPublicKey, lamports: amount))
                 transaction.message.recentBlockhash = recentBlockhash
                 try transaction.sign(signer: signer)
                 guard let serializedTransaction = try transaction.serialize().toBase64() else {
