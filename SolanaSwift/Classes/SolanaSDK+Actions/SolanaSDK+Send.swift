2f4d8cc21459cc3776406cade02c949e4e5c3165
Chung Tran <bigearsenal@gmail.com>
2021-02-16 20:25:48 +0700

    fix sendTokens

--- SolanaSwift/Classes/SolanaSDK+Actions/SolanaSDK+Send.swift
+++ SolanaSwift/Classes/SolanaSDK+Actions/SolanaSDK+Send.swift
@@ -9,7 +9,47 @@ import Foundation
 import RxSwift
 
 extension SolanaSDK {
-    public func sendTokens(
+    /**
+        send SOL to another account.
+     
+        - Parameter to: publicKey to send to
+        - Parameter amount: amount to send
+    */
+    public func sendSOL(
+        to toPublicKey: String,
+        amount: UInt64
+    ) -> Single<TransactionID> {
+        guard let account = self.accountStorage.account else {
+            return .error(Error.unauthorized)
+        }
+        
+        do {
+            let fromPublicKey = account.publicKey
+            let toPublicKey = try PublicKey(string: toPublicKey)
+            
+            var transaction = Transaction()
+            transaction.message.add(
+                instruction: SystemProgram.transferInstruction(
+                    from: fromPublicKey,
+                    to: toPublicKey,
+                    lamports: amount
+                )
+            )
+            
+            return serializeTransaction(transaction, signers: [account])
+                .flatMap {self.sendTransaction(serializedTransaction: $0)}
+        } catch {
+            return .error(error)
+        }
+    }
+    
+    /**
+        send SPLTokens to another account.
+     
+        - Parameter to: publicKey to send to
+        - Parameter amount: amount to send
+    */
+    public func sendSPLTokens(
         from fromPublicKey: String,
         to toPublicKey: String,
         amount: UInt64
@@ -22,54 +62,20 @@ extension SolanaSDK {
             let fromPublicKey = try PublicKey(string: fromPublicKey)
             let toPublicKey = try PublicKey(string: toPublicKey)
             
-            // Send SOL
-            if fromPublicKey == account.publicKey {
-                return sendSOL(account: account, from: fromPublicKey, to: toPublicKey, amount: amount)
-            }
-                
-            // Send Tokens
-            return sendSPLTokens(account: account, from: fromPublicKey, to: toPublicKey, amount: amount)
+            var transaction = Transaction()
+            transaction.message.add(
+                instruction: TokenProgram.transferInstruction(
+                    tokenProgramId: .tokenProgramId,
+                    source: fromPublicKey,
+                    destination: toPublicKey,
+                    owner: account.publicKey,
+                    amount: amount
+                )
+            )
+            return serializeTransaction(transaction, signers: [account])
+                .flatMap {self.sendTransaction(serializedTransaction: $0)}
         } catch {
             return .error(error)
         }
-    }
-    
-    private func sendSOL(
-        account: SolanaSDK.Account,
-        from fromPublicKey: PublicKey,
-        to toPublicKey: PublicKey,
-        amount: UInt64
-    ) -> Single<TransactionID> {
-        createSendTransaction(from: fromPublicKey, to: toPublicKey, amount: amount, signer: account)
-            .flatMap {self.sendTransaction(serializedTransaction: $0)}
-    }
-    
-    private func sendSPLTokens(
-        account: SolanaSDK.Account,
-        from fromPublicKey: PublicKey,
-        to toPublicKey: PublicKey,
-        amount: UInt64
-    ) -> Single<TransactionID> {
-        fatalError()
-    }
-    
-    public func createSendTransaction(
-        from fromPublicKey: PublicKey,
-        to toPublicKey: PublicKey,
-        amount: UInt64,
-        signer: Account,
-        recentBlockhash: String? = nil
-    ) -> Single<String> {
-        var transaction = Transaction()
-        transaction.message.add(
-            instruction: SystemProgram.transferInstruction(
-                from: fromPublicKey,
-                to: toPublicKey,
-                lamports: amount
-            )
-        )
-        
-        let serializedTransaction = serializeTransaction(transaction, recentBlockhash: recentBlockhash, signers: [signer])
-        return serializedTransaction
     }
 }
