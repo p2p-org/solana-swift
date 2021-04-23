364c6454a9aeaca0fb19290312db29247ffaee66
Chung Tran <bigearsenal@gmail.com>
2021-04-23 09:45:03 +0700

    fix toLamport

--- SolanaSwift/Classes/Extensions/Number+Extensions.swift
+++ SolanaSwift/Classes/Extensions/Number+Extensions.swift
@@ -35,4 +35,7 @@ extension Double {
     public func toLamport(decimals: Int) -> UInt64 {
         UInt64((self * pow(10, Double(decimals))).rounded())
     }
+    public func toLamport(decimals: UInt8) -> UInt64 {
+        toLamport(decimals: Int(decimals))
+    }
 }
