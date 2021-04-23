64af7ab8daf2f4516170e95cb04f27e309d8e09d
Chung Tran <bigearsenal@gmail.com>
2021-04-23 13:11:51 +0700

    fix liquidity

--- SolanaSwift/Classes/Models/Wallet.swift
+++ SolanaSwift/Classes/Models/Wallet.swift
@@ -57,7 +57,7 @@ extension SolanaSDK {
                     tags: [],
                     extensions: nil
                 ),
-                liquidity: true
+                liquidity: false
             )
         }
     }
