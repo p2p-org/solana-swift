6bc8822f43c0d229543a31c6cc577aaffc0d8bc6
Chung Tran <bigearsenal@gmail.com>
2021-04-28 10:18:15 +0700

    some fixes

--- SolanaSwift/Classes/Helpers/AskCoin-HD/ASKHDUtils.swift
+++ SolanaSwift/Classes/Helpers/AskCoin-HD/ASKHDUtils.swift
@@ -91,12 +91,10 @@ extension Data {
 // Copy from CryptoSwift/UInt32+Extension.swift
 extension UInt32 {
 	
-	@_specialize(exported: true, where T == ArraySlice<UInt8>)
 	init<T: Collection>(bytes: T) where T.Iterator.Element == UInt8, T.Index == Int {
 		self = UInt32(bytes: bytes, fromIndex: bytes.startIndex)
 	}
 	
-	@_specialize(exported: true, where T == ArraySlice<UInt8>)
 	init<T: Collection>(bytes: T, fromIndex index: T.Index) where T.Iterator.Element == UInt8, T.Index == Int {
 		let val0 = UInt32(bytes[index.advanced(by: 0)]) << 24
 		let val1 = UInt32(bytes[index.advanced(by: 1)]) << 16
