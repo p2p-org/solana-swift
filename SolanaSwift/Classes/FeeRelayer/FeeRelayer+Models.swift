2e8c8f06db10c771e03ec8191ae248aca06fb23c
Chung Tran <bigearsenal@gmail.com>
2021-05-12 10:39:39 +0700

    add fee-relayer

---
+++ SolanaSwift/Classes/FeeRelayer/FeeRelayer+Models.swift
@@ -0,0 +1,54 @@
+//
+//  FeeRelayer+Models.swift
+//  SolanaSwift
+//
+//  Created by Chung Tran on 12/05/2021.
+//
+
+import Foundation
+
+protocol SolanaFeeRelayerTransferParams: Encodable {
+    var sender: String {get}
+    var recipient: String {get}
+    var amount: SolanaSDK.Lamports {get}
+}
+
+extension SolanaSDK.FeeRelayer {
+    struct TransferSolParams: SolanaFeeRelayerTransferParams {
+        let sender: String
+        let recipient: String
+        let amount: SolanaSDK.Lamports
+        
+        enum CodingKeys: String, CodingKey {
+            case sender     =   "sender_pubkey"
+            case recipient  =   "recipient_pubkey"
+            case amount     =   "lamports"
+        }
+    }
+    
+    struct TransferSPLTokenParams: SolanaFeeRelayerTransferParams {
+        let sender: String
+        let recipient: String
+        let mintAddress: String
+        let authority: String
+        let amount: SolanaSDK.Lamports
+        let decimals: SolanaSDK.Decimals
+        let signature: String
+        let blockhash: String
+        
+        enum CodingKeys: String, CodingKey {
+            case sender         =   "sender_token_account_pubkey"
+            case recipient      =   "recipient_pubkey"
+            case mintAddress    =   "token_mint_pubkey"
+            case authority      =   "authority_pubkey"
+            case amount
+            case decimals
+        }
+    }
+    
+    struct InstructionsAndParams {
+        let instructions: [SolanaSDK.TransactionInstruction]
+        let transferParams: SolanaFeeRelayerTransferParams
+        let path: String
+    }
+}
---
+++ SolanaSwift/Classes/FeeRelayer/FeeRelayer.swift
@@ -0,0 +1,16 @@
+//
+//  FeeRelayer.swift
+//  SolanaSwift
+//
+//  Created by Chung Tran on 12/05/2021.
+//
+
+import Foundation
+import RxAlamofire
+
+extension SolanaSDK {
+    public struct FeeRelayer {
+        // MARK: - Constants
+    }
+}
+
