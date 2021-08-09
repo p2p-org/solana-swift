296fdb47e07f5d289bbef2d14695062bbbd07249
Chung Tran <bigearsenal@gmail.com>
2021-08-09 15:33:10 +0700

    some fixes

--- SolanaSwift/Classes/SerumSwap/SerumSwap.swift
+++ SolanaSwift/Classes/SerumSwap/SerumSwap.swift
@@ -8,18 +8,36 @@
 import Foundation
 import RxSwift
 
-protocol SerumSwapClient {
-    
+protocol SerumSwapAPIClient {
+    func getMarketAddressIfNeeded(fromMint: SolanaSDK.PublicKey, toMint: SolanaSDK.PublicKey) -> Single<SolanaSDK.PublicKey>
+    func getMinimumBalanceForRentExemption(span: UInt64) -> Single<UInt64>
+}
+
+protocol SerumSwapAccountProvider {
+    func getNativeWalletAddress() -> SolanaSDK.PublicKey
 }
 
 extension SolanaSDK {
     class SerumSwap {
+        // MARK: - Nested type
+        struct SignersAndInstructions {
+            let signers: [Account]
+            let instructions: [TransactionInstruction]
+        }
+        
+        // MARK: - Constants
+        let usdcMint = try! SolanaSDK.PublicKey(string: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+        let usdtMint = try! SolanaSDK.PublicKey(string: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB")
+        let dexPID = try! SolanaSDK.PublicKey(string: "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin")
+        
         // MARK: - Properties
-        let client: SerumSwapClient
+        let client: SerumSwapAPIClient
+        let accountProvider: SerumSwapAccountProvider
         
         // MARK: - Initializers
-        init(client: SerumSwapClient) {
+        init(client: SerumSwapAPIClient, accountProvider: SerumSwapAccountProvider) {
             self.client = client
+            self.accountProvider = accountProvider
         }
         
         /**
@@ -28,12 +46,71 @@ extension SolanaSDK {
          *
          * @throws if all open orders accounts already exist.
          */
-        public func initAccounts(from: PublicKey, to: PublicKey, feePayer: PublicKey, recentBlockhash: String) -> Single<PublicKey> {
+        public func initAccounts(
+            fromMint: PublicKey,
+            toMint: PublicKey
+        ) -> Single<PublicKey> {
             
-            var signers = [Account]()
-            var transaction = Transaction()
+            let request: Single<SignersAndInstructions>
             
+            // Direct swap on USD(x).
+            if fromMint == usdcMint || fromMint == usdtMint {
+                request = createAndInitAccountForDirectSwapOnUSDX(
+                    fromMint: fromMint,
+                    toMint: toMint
+                )
+            }
+            else if toMint == usdcMint || toMint == usdtMint {
+                request = createAndInitAccountForDirectSwapOnUSDX(
+                    fromMint: toMint,
+                    toMint: fromMint
+                )
+            }
             
+        }
+        
+        // MARK: - Helpers
+        private func createAndInitAccountForDirectSwapOnUSDX(
+            fromMint: PublicKey,
+            toMint: PublicKey
+        ) -> Single<SignersAndInstructions>
+        {
+            Single.zip(
+                client.getMarketAddressIfNeeded(
+                    fromMint: fromMint,
+                    toMint: toMint
+                ),
+                client.getMinimumBalanceForRentExemption(
+                    span: AccountInfo.span
+                )
+            )
+                .map {[weak self] marketAddress, minimumBalanceForRentExemption in
+                    guard let self = self else {throw SolanaSDK.Error.unknown}
+                    
+                    let openOrders = try Account(network: .mainnetBeta)
+                    
+                    // signers
+                    var signers = [Account]()
+                    signers.append(openOrders)
+                    
+                    // instructions
+                    var instructions = [TransactionInstruction]()
+                    
+                    instructions.append(
+                        SystemProgram.createAccountInstruction(
+                            from: self.accountProvider.getNativeWalletAddress(),
+                            toNewPubkey: openOrders.publicKey,
+                            lamports: minimumBalanceForRentExemption,
+                            programPubkey: self.dexPID
+                        )
+                    )
+                    
+                    instructions.append(
+                        TokenProgram.initializeAccountInstruction(programId: <#T##SolanaSDK.PublicKey#>, account: <#T##SolanaSDK.PublicKey#>, mint: <#T##SolanaSDK.PublicKey#>, owner: <#T##SolanaSDK.PublicKey#>)
+                    )
+                    
+                    return .init(signers: signers, instructions: instructions)
+                }
         }
     }
 }
