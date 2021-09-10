5a7d4ec9c3942e47a81b10b826eebcee5691ecf0
Chung Tran <bigearsenal@gmail.com>
2021-09-10 15:14:18 +0700

    some fixes

--- SolanaSwift/Classes/RenVMBridge/RenVM+RpcClient.swift
+++ SolanaSwift/Classes/RenVMBridge/RenVM+RpcClient.swift
@@ -12,4 +12,9 @@ public protocol RenVMRpcClientType {
     init(_ network: RenVM.Network)
     func call<T: Decodable>(endpoint: String, params: Encodable) -> Single<T>
     func getAccountInfo<T: DecodableBufferLayout>(account: String, decodedTo: T.Type) -> Single<SolanaSDK.BufferInfo<T>>
+    func getMintData(
+        mintAddress: String,
+        programId: String
+    ) -> Single<SolanaSDK.Mint>
+    func getConfirmedSignaturesForAddress2(account: String, configs: SolanaSDK.RequestConfiguration?) -> Single<[SolanaSDK.SignatureInfo]>
 }
--- SolanaSwift/Classes/RenVMBridge/RenVM+SolanaChain.swift
+++ SolanaSwift/Classes/RenVMBridge/RenVM+SolanaChain.swift
@@ -40,8 +40,149 @@ extension RenVM {
             }
         }
         
-        public func resolveTokenGatewayContract() throws {
-            fatalError()
+        func resolveTokenGatewayContract() throws -> SolanaSDK.PublicKey {
+            guard let sHash = try? SolanaSDK.PublicKey(string: Base58.encode(Hash.generateSHash().bytes)),
+                let index = gatewayRegistryData.selectors.firstIndex(of: sHash),
+                gatewayRegistryData.gateways.count > index
+            else {throw Error("Could not resolve token gateway contract")}
+            return gatewayRegistryData.gateways[index]
+        }
+        
+        func getSPLTokenPubkey() throws -> SolanaSDK.PublicKey {
+            let program = try resolveTokenGatewayContract()
+            let sHash = Hash.generateSHash()
+            return try .findProgramAddress(seeds: [sHash], programId: program).0
+        }
+        
+        func getAssociatedTokenAddress(
+            address: SolanaSDK.PublicKey
+        ) throws -> SolanaSDK.PublicKey {
+            let tokenMint = try getSPLTokenPubkey()
+            return try .associatedTokenAddress(walletAddress: address, tokenMintAddress: tokenMint)
+        }
+        
+//        public String createAssociatedTokenAccount(PublicKey address, Account signer) throws Exception {
+//            PublicKey tokenMint = getSPLTokenPubkey();
+//            PublicKey associatedTokenAddress = getAssociatedTokenAddress(address);
+//
+//            TransactionInstruction createAccountInstruction = TokenProgram.createAssociatedTokenAccountInstruction(
+//                    TokenProgram.ASSOCIATED_TOKEN_PROGRAM_ID, TokenProgram.PROGRAM_ID, tokenMint, associatedTokenAddress,
+//                    address, signer.getPublicKey());
+//
+//            Transaction transaction = new Transaction();
+//            transaction.addInstruction(createAccountInstruction);
+//
+//            return client.getApi().sendTransaction(transaction, signer);
+//        }
+//
+//        public String submitMint(PublicKey address, Account signer, ResponseQueryTxMint responceQueryMint)
+//                throws Exception {
+//            byte[] pHash = Utils.fromURLBase64(responceQueryMint.getValueIn().phash);
+//            String amount = responceQueryMint.getValueOut().amount;
+//            byte[] nHash = Utils.fromURLBase64(responceQueryMint.getValueIn().nhash);
+//            byte[] sig = Utils.fixSignatureSimple(responceQueryMint.getValueOut().sig);
+//
+//            PublicKey program = resolveTokenGatewayContract();
+//            PublicKey gatewayAccountId = PublicKey.findProgramAddress(Arrays.asList(GatewayStateKey.getBytes()), program)
+//                    .getAddress();
+//            byte[] sHash = Hash.generateSHash();
+//            PublicKey tokenMint = getSPLTokenPubkey();
+//            PublicKey mintAuthority = PublicKey.findProgramAddress(Arrays.asList(tokenMint.toByteArray()), program)
+//                    .getAddress();
+//            PublicKey recipientTokenAccount = getAssociatedTokenAddress(address);
+//
+//            byte[] renVMMessage = buildRenVMMessage(pHash, amount, sHash, recipientTokenAccount.toByteArray(), nHash);
+//            PublicKey mintLogAccount = PublicKey.findProgramAddress(Arrays.asList(Hash.keccak256(renVMMessage)), program)
+//                    .getAddress();
+//
+//            TransactionInstruction mintInstruction = RenProgram.mintInstruction(signer.getPublicKey(), gatewayAccountId,
+//                    tokenMint, recipientTokenAccount, mintLogAccount, mintAuthority, program);
+//
+//            AccountInfo gatewayInfo = client.getApi().getAccountInfo(gatewayAccountId);
+//            String base64Data = gatewayInfo.getValue().getData().get(0);
+//            GatewayStateData gatewayState = GatewayStateData.decode(Base64.getDecoder().decode(base64Data));
+//
+//            TransactionInstruction secpInstruction = RenProgram.createInstructionWithEthAddress2(
+//                    gatewayState.renVMAuthority, renVMMessage, Arrays.copyOfRange(sig, 0, 64), sig[64] - 27);
+//
+//            Transaction transaction = new Transaction();
+//            transaction.addInstruction(mintInstruction);
+//            transaction.addInstruction(secpInstruction);
+//
+//            String confirmedSignature = client.getApi().sendTransaction(transaction, signer);
+//
+//            return confirmedSignature;
+//        }
+//
+//        public String findMintByDepositDetails(byte[] nHash, byte[] pHash, byte[] to, String amount) throws Exception {
+//            PublicKey program = resolveTokenGatewayContract();
+//            byte[] sHash = Hash.generateSHash();
+//            byte[] renVMMessage = buildRenVMMessage(pHash, amount, sHash, new PublicKey(to).toByteArray(), nHash);
+//            PublicKey mintLogAccount = PublicKey.findProgramAddress(Arrays.asList(Hash.keccak256(renVMMessage)), program)
+//                    .getAddress();
+//
+//            String signature = "";
+//            try {
+//                MintData mintData = Token.getMintData(client, mintLogAccount, program);
+//                if (!mintData.isInitialized()) {
+//                    return signature;
+//                }
+//
+//                List<SignatureInformation> signatures = client.getApi().getConfirmedSignaturesForAddress2(mintLogAccount,
+//                        1);
+//                signature = signatures.get(0).getSignature();
+//            } catch (Exception e) {
+//            }
+//
+//            return signature;
+//        }
+        public func findMintByDepositDetail(
+            nHash: Data,
+            pHash: Data,
+            to: SolanaSDK.PublicKey,
+            amount: String
+        ) throws -> Single<String> {
+            let program = try resolveTokenGatewayContract()
+            let sHash = Hash.generateSHash()
+            let renVMMessage = try Self.buildRenVMMessage(pHash: pHash, amount: amount, token: sHash, to: to, nHash: nHash)
+            
+            let mintLogAccount = try SolanaSDK.PublicKey.findProgramAddress(seeds: [renVMMessage.keccak256], programId: program).0
+            return client.getMintData(mintAddress: mintLogAccount.base58EncodedString, programId: program.base58EncodedString)
+                .flatMap {mint -> Single<String> in
+                    if !mint.isInitialized {return .just("")}
+                    return client.getConfirmedSignaturesForAddress2(
+                        account: mintLogAccount.base58EncodedString,
+                        configs: nil
+                    )
+                        .map {$0.first?.signature ?? ""}
+                }
+        }
+        
+        // MARK: - Static methods
+        public static func buildRenVMMessage(
+            pHash: Data,
+            amount: String,
+            token: Data,
+            to: SolanaSDK.PublicKey,
+            nHash: Data
+        ) throws -> Data {
+            // serialize amount
+            let amount = BInt(amount)
+            let amountBytes = amount.data.bytes
+            guard amountBytes.count <= 32 else {
+                throw Error("The amount is not valid")
+            }
+            var amountData = Data(repeating: 0, count: 32 - amountBytes.count)
+            amountData += amountBytes
+            
+            // form data
+            var data = Data()
+            data += pHash
+            data += amountData
+            data += token
+            data += to.data
+            data += nHash
+            return data
         }
     }
 }
