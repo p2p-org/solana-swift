4cab3a11025b87173392746bcd82822d9550de80
Chung Tran <bigearsenal@gmail.com>
2021-09-14 17:06:24 +0700

    remove comment

--- Example/Tests/RenVMBurnAndReleaseTests.swift
+++ Example/Tests/RenVMBurnAndReleaseTests.swift
@@ -30,14 +30,4 @@ class RenVMBurnAndReleaseTests: XCTestCase {
         
         XCTAssertEqual(burnState.txHash, "I_HJMksqVC5_-0G9FE_z8AORRDMoxl1vZbSGEc2VfJ4")
     }
-//    @Test
-//    public void getBurnStateTest() throws Exception {
-//        BurnAndRelease burnAndRelease = new BurnAndRelease(NetworkConfig.TESTNET());
-//        BurnDetails burnDetails = new BurnDetails();
-//        burnDetails.confirmedSignature = "2kNe8duPRcE9xxKLLVP92e9TBH5WvmVVWQJ18gEjqhgxsrKtBEBVfeXNFz5Un3yEEQJZkxY2ysQR4dGQaytnDM1i";
-//        burnDetails.nonce = BigInteger.valueOf(35);
-//        burnDetails.recepient = "tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt";
-//        assertEquals("I_HJMksqVC5_-0G9FE_z8AORRDMoxl1vZbSGEc2VfJ4",
-//                burnAndRelease.getBurnState(burnDetails, "1000").txHash);
-//    }
 }
