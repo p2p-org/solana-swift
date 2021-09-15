aed960c1db487f47de6a0e57ec90160524fbbda7
Chung Tran <bigearsenal@gmail.com>
2021-09-15 11:16:10 +0700

    fix testEncodeBody

--- Example/Tests/RenVMRPCClientTests.swift
+++ Example/Tests/RenVMRPCClientTests.swift
@@ -44,6 +44,6 @@ class RenVMRPCClientTests: XCTestCase {
             params: .init(wrapped: ["tx": tx])
         )
         
-        Logger.log(message: body.jsonString!, event: .info)
+        XCTAssertEqual(body.jsonString, #"{"id":1,"jsonrpc":"2.0","method":"ren_submitTx","params":{"tx":{"hash":"nrRusEWs3bn619zTGc1937EakvCGvjbt5gYr4L2PL_M","selector":"BTC\/fromSolana","in":{"t":{"struct":[{"txid":"bytes"},{"txindex":"u32"},{"amount":"u256"},{"payload":"bytes"},{"phash":"bytes32"},{"to":"string"},{"nonce":"bytes32"},{"nhash":"bytes32"},{"gpubkey":"bytes"},{"ghash":"bytes32"}]},"v":{"txindex":"0","txid":"YtU3AP9wspScgOI6kgDr1gp49AbS52Mio7Q8JltutDJhgGSz3qkM20Csti1PRGpsJUwYHuqeWBNY_ySoUo_CCw","ghash":"Bde-qbf54lElW4RIPc6GkbBkZ0muCAiIL1CEe5rB1Y8","amount":"10000","nhash":"Y_dfWQXxRLYMBs8T0S7SVjeh4hdTPzIDpxXUfBJRK2k","payload":"","phash":"xdJGAYb3IzySfn2y3McDwOUAtlPKgic7e_rYBF2FpHA","to":"tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt","gpubkey":"","nonce":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABU"}},"version":"1"}}}"#)
     }
 }
