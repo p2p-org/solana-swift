81abaa79d2bfcd20026f6e626ae257af1b38403c
Chung Tran <bigearsenal@gmail.com>
2021-10-13 15:09:36 +0700

    implement findRoutes

--- Sources/SolanaSwift/OrcaSwap/OrcaSwap.swift
+++ Sources/SolanaSwift/OrcaSwap/OrcaSwap.swift
@@ -6,7 +6,109 @@
 //
 
 import Foundation
+import RxSwift
 
 public struct OrcaSwap {
+    // MARK: - Properties
+    let apiClient: OrcaSwapAPIClient
     
+    // MARK: - Methods
+    public func findRoutes() -> Single<Routes> {
+        Single.zip(
+            apiClient.getTokens(),
+            apiClient.getPools()
+        )
+        .map {tokens, pools in
+            let tokens = tokens.filter {$0.value.poolToken == false}
+                .map {$0.key}
+            let pairs = getPairs(tokens: tokens)
+            return getAllRoutes(pairs: pairs, pools: pools)
+        }
+    }
+}
+
+// MARK: - Helpers
+private func getPairs(tokens: [String]) -> [[String]] {
+    var pairs = [[String]]()
+    
+    for i in 0..<tokens.count-1 {
+        for j in i+1..<tokens.count {
+            let tokenA = tokens[i]
+            let tokenB = tokens[j]
+            
+            pairs.append(orderTokenPair(tokenA, tokenB))
+        }
+    }
+    
+    return pairs
+}
+
+private func orderTokenPair(_ tokenX: String, _ tokenY: String) -> [String] {
+    if (tokenX == "USDC" && tokenY == "USDT") {
+        return [tokenX, tokenY];
+    } else if (tokenY == "USDC" && tokenX == "USDT") {
+        return [tokenY, tokenX];
+    } else if (tokenY == "USDC" || tokenY == "USDT") {
+        return [tokenX, tokenY];
+    } else if (tokenX == "USDC" || tokenX == "USDT") {
+        return [tokenY, tokenX];
+    } else if tokenX < tokenY {
+        return [tokenX, tokenY];
+    } else {
+        return [tokenY, tokenX];
+    }
+}
+
+private func getAllRoutes(pairs: [[String]], pools: OrcaSwap.Pools) -> OrcaSwap.Routes {
+    var routes: OrcaSwap.Routes = [:]
+    pairs.forEach { pair in
+        guard let tokenA = pair.first,
+              let tokenB = pair.last
+        else {return}
+        routes[getTradeId(tokenA, tokenB)] = getRoutes(tokenA: tokenA, tokenB: tokenB, pools: pools)
+    }
+    return routes
+}
+
+private func getTradeId(_ tokenX: String, _ tokenY: String) -> String {
+    orderTokenPair(tokenX, tokenY).joined(separator: "/")
+}
+
+private func getRoutes(tokenA: String, tokenB: String, pools: OrcaSwap.Pools) -> [OrcaSwap.Route] {
+    var routes = [OrcaSwap.Route]()
+    
+    // Find all pools that contain the same tokens.
+    // Checking tokenAName and tokenBName will find Stable pools.
+    for (poolId, poolConfig) in pools {
+        if (poolConfig.tokenAName == tokenA && poolConfig.tokenBName == tokenB) ||
+            (poolConfig.tokenAName == tokenB && poolConfig.tokenBName == tokenA)
+        {
+            routes.append([poolId])
+        }
+    }
+    
+    // Find all pools that contain the first token but not the second
+    let firstLegPools = pools
+        .filter {
+            ($0.value.tokenAName == tokenA && $0.value.tokenBName != tokenB) ||
+                ($0.value.tokenBName == tokenA && $0.value.tokenAName != tokenB)
+        }
+        .reduce([String: String]()) { result, pool in
+            var result = result
+            result[pool.key] = pool.value.tokenBName == tokenA ? pool.value.tokenAName: pool.value.tokenBName
+            return result
+        }
+    
+    // Find all routes that can include firstLegPool and a second pool.
+    firstLegPools.forEach { firstLegPoolId, intermediateTokenName in
+        pools.forEach { secondLegPoolId, poolConfig in
+            if (poolConfig.tokenAName == intermediateTokenName && poolConfig.tokenBName == tokenB) ||
+                (poolConfig.tokenBName == intermediateTokenName && poolConfig.tokenAName == tokenB)
+            {
+                routes.append([firstLegPoolId, secondLegPoolId])
+            }
+        }
+    }
+    
+    return routes
 }
