723dba823deb1a5ca475127bd089c939756ebd12
Chung Tran <bigearsenal@gmail.com>
2021-10-14 08:54:37 +0700

    fix tests

--- Tests/SolanaSwiftTests/Swap/OrcaSwap/OrcaSwapRoutesTests.swift
+++ Tests/SolanaSwiftTests/Swap/OrcaSwap/OrcaSwapRoutesTests.swift
@@ -40,6 +40,7 @@ class OrcaSwapRoutesTests: XCTestCase {
     }
     
     func testGetPools() throws {
+        // Order may change
 //        [
 //            [
 //                "BTC/ETH"
@@ -58,25 +59,25 @@ class OrcaSwapRoutesTests: XCTestCase {
         XCTAssertEqual(pools.count, 3) //
         XCTAssertEqual(pools.flatMap { $0 }.count, 5)
         
-        let btcETHPool = pools[0][0]
+        let btcETHPool = pools.first(where: {$0.count == 1})!.first!
         XCTAssertEqual(btcETHPool.tokenAccountA, "81w3VGbnszMKpUwh9EzAF9LpRzkKxc5XYCW64fuYk1jH")
         XCTAssertEqual(btcETHPool.tokenAccountB, "6r14WvGMaR1xGMnaU8JKeuDK38RvUNxJfoXtycUKtC7Z")
         XCTAssertEqual(btcETHPool.tokenAName, "BTC")
         XCTAssertEqual(btcETHPool.tokenBName, "ETH")
         
-        let btcSOLAquafarm = pools[1][0]
+        let btcSOLAquafarm = pools.first(where: {$0.contains(where: {$0.account == "7N2AEJ98qBs4PwEwZ6k5pj8uZBKMkZrKZeiC7A64B47u"})})!.first!
         XCTAssertEqual(btcSOLAquafarm.tokenAccountA, "9G5TBPbEUg2iaFxJ29uVAT8ZzxY77esRshyHiLYZKRh8")
         XCTAssertEqual(btcSOLAquafarm.tokenAccountB, "5eqcnUasgU2NRrEAeWxvFVRTTYWJWfAJhsdffvc6nJc2")
         XCTAssertEqual(btcSOLAquafarm.tokenAName, "BTC")
         XCTAssertEqual(btcSOLAquafarm.tokenBName, "SOL")
         
-        let ethSOL = pools[1][1] // reversed to SOL/ETH
+        let ethSOL = pools.first(where: {$0.contains(where: {$0.account == "4vWJYxLx9F7WPQeeYzg9cxhDeaPjwruZXCffaSknWFxy"})})!.last! // Reversed to SOL/ETH
         XCTAssertEqual(ethSOL.tokenAccountA, "5x1amFuGMfUVzy49Y4Pc3HyCVD2usjLaofnzB3d8h7rv") // originalTokenAccountB
         XCTAssertEqual(ethSOL.tokenAccountB, "FidGus13X2HPzd3cuBEFSq32UcBQkF68niwvP6bM4fs2") // originalTokenAccountA
         XCTAssertEqual(ethSOL.tokenAName, "SOL")
         XCTAssertEqual(ethSOL.tokenBName, "ETH")
         
-        let ethSOLAquafarm = pools[2][1] // reversed to SOL/ETH
+        let ethSOLAquafarm = pools.first(where: {$0.contains(where: {$0.account == "EuK3xDa4rWuHeMQCBsHf1ETZNiEQb5C476oE9u9kp8Ji"})})!.last! // reversed to SOL/ETH
         XCTAssertEqual(ethSOLAquafarm.tokenAccountA, "5pUTGvN2AA2BEzBDU4CNDh3LHER15WS6J8oJf5XeZFD8") // originalTokenAccountB
         XCTAssertEqual(ethSOLAquafarm.tokenAccountB, "7F2cLdio3i6CCJaypj9VfNDPW2DwT3vkDmZJDEfmxu6A") // originalTokenAccountA
         XCTAssertEqual(ethSOLAquafarm.tokenAName, "SOL")
