dcb4a675d4036d4bd6e515f9c4c9bb367c3a494d
Chung Tran <bigearsenal@gmail.com>
2021-10-18 15:58:04 +0700

    add some direct tests

--- Tests/SolanaSwiftTests/Swap/OrcaSwap/Helpers/OrcaConstants.swift
+++ Tests/SolanaSwiftTests/Swap/OrcaSwap/Helpers/OrcaConstants.swift
@@ -6,7 +6,125 @@
 //
 
 import Foundation
+@testable import SolanaSwift
 
 let btcMint = "9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E"
 let ethMint = "2FPyTwcZLUg1MDrwsyoP4D6s1tM7hAkHYRjkNb5w6Pxk"
 let socnMint = "5oVNBeEEQvYi1cX3ir8Dx5n1P7pdxydbGF2X4TxVusJm"
+let solMint = "So11111111111111111111111111111111111111112"
+let ninjaMint = "FgX1WD9WzMU3yLwXaFSarPfkgzjLb2DZCqmkx9ExpuvJ"
+let usdcMint = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+
+let socnSOLStableAquafarmsPool = OrcaSwap.Pool(
+    account: "2q6UMko5kTnv866W9MTeAFau94pLpsdeNjDdSYSgZUXr",
+    authority: "Gyd77CwV23qq937x9UDa4TDkxEeQF9tp8ifotYxqW3Kd",
+    nonce: 255,
+    poolTokenMint: "APNpzQvR91v1THbsAyG3HHrUEwvexWYeNCFLQuVnxgMc",
+    tokenAccountA: "C8DRXUqxXtUgvgBR7BPAmy6tnRJYgVjG27VU44wWDMNV",
+    tokenAccountB: "DzdxH5qJ68PiM1p5o6PbPLPpDj8m1ZshcaMFATcxDZix",
+    feeAccount: "42Xzazs9EvjtidvEDrj3JXbDtf6fpTq5XHh96mPctvBV",
+    hostFeeAccount: nil,
+    feeNumerator: 6,
+    feeDenominator: 10000,
+    ownerTradeFeeNumerator: 1,
+    ownerTradeFeeDenominator: 10000,
+    ownerWithdrawFeeNumerator: 0,
+    ownerWithdrawFeeDenominator: 0,
+    hostFeeNumerator: 0,
+    hostFeeDenominator: 0,
+    tokenAName: "SOCN",
+    tokenBName: "SOL",
+    curveType: "Stable",
+    amp: 100,
+    programVersion: 2,
+    deprecated: nil,
+    tokenABalance: .init(
+        uiAmount: 20548.358418788,
+        amount: "20548358418788",
+        decimals: 9,
+        uiAmountString: "20548.358418788"
+    ),
+    tokenBBalance: .init(
+        uiAmount: 26424.277517753,
+        amount: "26424277517753",
+        decimals: 9,
+        uiAmountString: "26424.277517753"
+    ),
+    isStable: true
+)
+
+let solNinjaAquafarmsPool = OrcaSwap.Pool(
+    account: "3ECUtPokme1nimJfuAkMtcm7QYjDEfXRQzmGC16LuYnz",
+    authority: "H8f9n2PfehUc73gRWSRTPXvqUhUHVywdAxcfEtYmmyAo",
+    nonce: 255,
+    poolTokenMint: "4X1oYoFWYtLebk51zuh889r1WFLe8Z9qWApj87hQMfML",
+    tokenAccountA: "9SxzphwrrDVDkwkyvmtag9NLgpjSkTw35cRwg9rLMYWk",
+    tokenAccountB: "6Y9VyEYHgxVahiixzphNh4HAywpab9zVoD4S8q1sfuL8",
+    feeAccount: "43ViAbUVujnYtJyzGP4AhabMYCbLsExenT3WKsZjqJ7N",
+    hostFeeAccount: nil,
+    feeNumerator: 25,
+    feeDenominator: 10000,
+    ownerTradeFeeNumerator: 5,
+    ownerTradeFeeDenominator: 10000,
+    ownerWithdrawFeeNumerator: 0,
+    ownerWithdrawFeeDenominator: 0,
+    hostFeeNumerator: 0,
+    hostFeeDenominator: 0,
+    tokenAName: "SOL",
+    tokenBName: "NINJA",
+    curveType: "ConstantProduct",
+    amp: nil,
+    programVersion: 2,
+    deprecated: nil,
+    tokenABalance: .init(
+        uiAmount: 19449.398641374,
+        amount: "19449398641374",
+        decimals: 9,
+        uiAmountString: "19449.398641374"
+    ),
+    tokenBBalance: .init(
+        uiAmount: 1796762.444462,
+        amount: "1796762444462",
+        decimals: 6,
+        uiAmountString: "1796762.444462"
+    ),
+    isStable: nil
+)
+
+let socnUSDCAquafarmsPool = OrcaSwap.Pool(
+    account: "6Gh36sNXrGWYiWr999d9iZtqgnipJbWuBohyHBN1cJpS",
+    authority: "GXWEpRURaQZ9E62Q23EreTUfBy4hfemXgWFUWcg7YFgv",
+    nonce: 255,
+    poolTokenMint: "Dkr8B675PGnNwEr9vTKXznjjHke5454EQdz3iaSbparB",
+    tokenAccountA: "7xs9QsrxQDVoWQ8LQ8VsVjfPKBrPGjvg8ZhaLnU1i2VR",
+    tokenAccountB: "FZFJK64Fk1t619zmVPqCx8Uy29zJ3WuvjWitCQuxXRo3",
+    feeAccount: "HsC1Jo38jK3EpoNAkxfoUJhQVPa28anewZpLfeouUNk7",
+    hostFeeAccount: nil,
+    feeNumerator: 25,
+    feeDenominator: 10000,
+    ownerTradeFeeNumerator: 5,
+    ownerTradeFeeDenominator: 10000,
+    ownerWithdrawFeeNumerator: 0,
+    ownerWithdrawFeeDenominator: 0,
+    hostFeeNumerator: 0,
+    hostFeeDenominator: 0,
+    tokenAName: "SOCN",
+    tokenBName: "USDC",
+    curveType: "ConstantProduct",
+    amp: nil,
+    programVersion: 2,
+    deprecated: nil,
+    tokenABalance: .init(
+        uiAmount: 3477.492966425,
+        amount: "3477492966425",
+        decimals: 9,
+        uiAmountString: "3477.492966425"
+    ),
+    tokenBBalance: .init(
+        uiAmount: 554749.837968,
+        amount: "554749837968",
+        decimals: 6,
+        uiAmountString: "554749.837968"
+    ),
+    isStable: nil
+)
--- Tests/SolanaSwiftTests/Swap/OrcaSwap/OrcaSwapIntegrationTests.swift
+++ Tests/SolanaSwiftTests/Swap/OrcaSwap/OrcaSwapIntegrationTests.swift
@@ -9,15 +9,102 @@ import Foundation
 import XCTest
 @testable import SolanaSwift
 
+private let socnPubkey = "64DzCPdUpQUTnSgY6hP6ux125vY2v3aWbE4T4G42SM1j"
+private let solPubkey = "3h1zGmCwsRJnVk5BuRNMLsPaQu1y2aqXqXDWYCgrp5UG"
+private let usdcPubkey = "3uetDDizgTtadDHZzyy9BqxrjQcozMEkxzbKhfZF4tG3"
+
 class OrcaSwapIntegrationTests: XCTestCase {
-    let solanaSDK = SolanaSDK(
-        endpoint: .init(url: "https://p2p.rpcpool.com/", network: .mainnetBeta),
-        accountStorage: InMemoryAccountStorage()
-    )
+    var solanaSDK: SolanaSDK!
+    var orcaSwap: OrcaSwap!
     
-    private lazy var orcaSwap = OrcaSwap(
-        apiClient: OrcaSwap.MockAPIClient(network: "mainnet"),
-        solanaClient: solanaSDK,
-        accountProvider: solanaSDK
-    )
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        let accountStorage = InMemoryAccountStorage()
+        
+        let endpoint = SolanaSDK.APIEndPoint(url: "https://p2p.rpcpool.com/", network: .mainnetBeta)
+        
+        solanaSDK = SolanaSDK(
+            endpoint: endpoint,
+            accountStorage: accountStorage
+        )
+        
+        let account = try SolanaSDK.Account(
+            phrase: "miracle pizza supply useful steak border same again youth silver access hundred".components(separatedBy: " "),
+            network: endpoint.network
+        )
+        try accountStorage.save(account)
+        
+        orcaSwap = OrcaSwap(
+            apiClient: OrcaSwap.MockAPIClient(network: "mainnet"),
+            solanaClient: solanaSDK,
+            accountProvider: solanaSDK
+        )
+        
+        _ = orcaSwap.load().toBlocking().materialize()
+    }
+    
+    // MARK: - Direct SOL to SPL
+    func testDirectSwapSOLToCreatedSPL() throws {
+        let amount: Double = 0.001 // 0.001 SOL to created SOCN
+        
+        let swapSimulation = orcaSwap.swap(
+            fromWalletPubkey: solPubkey,
+            toWalletPubkey: socnPubkey,
+            bestPoolsPair: [socnSOLStableAquafarmsPool.reversed],
+            amount: amount,
+            slippage: 0.005,
+            isSimulation: true
+        )
+        
+        XCTAssertNoThrow(try swapSimulation.toBlocking().first())
+    }
+    
+    func testDirectSwapSOLToUncreatedSPL() throws {
+        let amount: Double = 0.001 // 0.001 SOL to uncreated
+        
+        let swapSimulation = orcaSwap.swap(
+            fromWalletPubkey: solPubkey,
+            toWalletPubkey: nil,
+            bestPoolsPair: [solNinjaAquafarmsPool],
+            amount: amount,
+            slippage: 0.005,
+            isSimulation: true
+        )
+        
+        XCTAssertNoThrow(try swapSimulation.toBlocking().first())
+    }
+    
+    // MARK: - Direct SPL to SOL
+    func testDirectSwapSPLToSOL() throws {
+        let amount: Double = 0.001 // 0.001 SOCN to Native SOL
+        
+        let swapSimulation = orcaSwap.swap(
+            fromWalletPubkey: socnPubkey,
+            toWalletPubkey: solPubkey,
+            bestPoolsPair: [socnSOLStableAquafarmsPool],
+            amount: amount,
+            slippage: 0.005,
+            isSimulation: true
+        )
+        
+        XCTAssertNoThrow(try swapSimulation.toBlocking().first())
+    }
+    
+    // MARK: - Direct SPL to SPL
+    func testDirectSwapSPLToCreatedSPL() throws {
+        let amount: Double = 0.001 // 0.001 SOCN to USDC
+        
+        let swapSimulation = orcaSwap.swap(
+            fromWalletPubkey: socnPubkey,
+            toWalletPubkey: usdcPubkey,
+            bestPoolsPair: [socnUSDCAquafarmsPool],
+            amount: amount,
+            slippage: 0.005,
+            isSimulation: true
+        )
+        
+        XCTAssertNoThrow(try swapSimulation.toBlocking().first())
+    }
 }
+
