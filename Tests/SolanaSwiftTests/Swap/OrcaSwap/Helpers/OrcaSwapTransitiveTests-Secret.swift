f765e76f91fb948ba93a4882e040c18ab8d6e79d
Tran Giang Long <trangianglong4003@gmail.com>
2021-11-21 22:44:51 +0100

    fix(github-action): optional

--- Tests/SolanaSwiftTests/Swap/OrcaSwap/Helpers/OrcaSwapTransitiveTests-Secret.swift
+++ Tests/SolanaSwiftTests/Swap/OrcaSwap/Helpers/OrcaSwapTransitiveTests-Secret.swift
@@ -9,14 +9,14 @@ import Foundation
 
 extension OrcaSwapTransitiveTests {
     var kuroPubkey: String {
-        ProcessInfo.processInfo.environment["KURO_PUB_KEY"]
+        ProcessInfo.processInfo.environment["KURO_PUB_KEY"] ?? ""
     }
     
     var secretPhrase: String {
-        ProcessInfo.processInfo.environment["SECRET_PHRASE"]
+        ProcessInfo.processInfo.environment["SECRET_PHRASE"] ?? ""
     }
     
     var slimPubkey: String {
-        ProcessInfo.processInfo.environment["SLIM_PUB_KEY"]
+        ProcessInfo.processInfo.environment["SLIM_PUB_KEY"] ?? ""
     }
 }
