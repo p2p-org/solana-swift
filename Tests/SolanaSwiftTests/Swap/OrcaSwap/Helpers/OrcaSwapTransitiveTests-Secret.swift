c456aac143d9577efcafb5a224611edcba89d8c5
Tran Giang Long <trangianglong4003@gmail.com>
2021-11-21 22:23:31 +0100

    feat(github-action): use env variables insteed harcoded variables

---
+++ Tests/SolanaSwiftTests/Swap/OrcaSwap/Helpers/OrcaSwapTransitiveTests-Secret.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chung Tran on 19/10/2021.
+//
+
+import Foundation
+
+extension OrcaSwapTransitiveTests {
+    var kuroPubkey: String {
+        ProcessInfo.processInfo.environment["KURO_PUB_KEY"]
+    }
+    
+    var secretPhrase: String {
+        ProcessInfo.processInfo.environment["SECRET_PHRASE"]
+    }
+    
+    var slimPubkey: String {
+        ProcessInfo.processInfo.environment["SLIM_PUB_KEY"]
+    }
+}
