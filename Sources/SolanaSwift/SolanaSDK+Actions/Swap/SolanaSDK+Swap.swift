16f4290982dcb5489590788199583a26c0c4d8f2
Chung Tran <bigearsenal@gmail.com>
2022-01-24 11:57:18 +0700

    feat: expose some APIs

--- Sources/SolanaSwift/SolanaSDK+Actions/Swap/SolanaSDK+Swap.swift
+++ Sources/SolanaSwift/SolanaSDK+Actions/Swap/SolanaSDK+Swap.swift
@@ -12,6 +12,11 @@ extension SolanaSDK {
     public struct SwapResponse {
         public let transactionId: String
         public let newWalletPubkey: String?
+        
+        public init(transactionId: String, newWalletPubkey: String?) {
+            self.transactionId = transactionId
+            self.newWalletPubkey = newWalletPubkey
+        }
     }
     
     public struct AccountInstructions {
