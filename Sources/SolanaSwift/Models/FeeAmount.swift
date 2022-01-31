6407910a7590c1cdb219a6f6915158dac25b8fdb
Chung Tran <bigearsenal@gmail.com>
2022-01-31 08:20:08 +0700

    feat: OtherFee

--- Sources/SolanaSwift/Models/FeeAmount.swift
+++ Sources/SolanaSwift/Models/FeeAmount.swift
@@ -2,13 +2,25 @@ import Foundation
 
 extension SolanaSDK {
     public struct FeeAmount: Equatable {
-        public init(transaction: UInt64, accountBalances: UInt64) {
+        public struct OtherFee: Equatable {
+            public init(amount: Double, unit: String) {
+                self.amount = amount
+                self.unit = unit
+            }
+            
+            public var amount: Double
+            public var unit: String
+        }
+        
+        public init(transaction: UInt64, accountBalances: UInt64, others: [OtherFee]? = nil) {
             self.transaction = transaction
             self.accountBalances = accountBalances
+            self.others = others
         }
         
         public var transaction: UInt64
         public var accountBalances: UInt64
+        public var others: [OtherFee]?
         public var total: UInt64 {
             transaction + accountBalances
         }
