49510c8516939e31434c666a2600760f3e252591
Chung Tran <bigearsenal@gmail.com>
2022-04-13 10:33:47 +0700

    feat: concurrency extension

--- Sources/SolanaSwift/APIClient/SolanaSDK.APIClient.swift
+++ Sources/SolanaSwift/APIClient/SolanaSDK.APIClient.swift
@@ -109,36 +109,3 @@ extension SolanaSDK {
         }
     }
 }
-
-@available(macOS, deprecated: 12.0, message: "Use the built-in API instead")
-extension URLSession {
-    func data(from url: URL) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: url) { data, response, error in
-                guard let data = data, let response = response else {
-                    let error = error ?? URLError(.badServerResponse)
-                    return continuation.resume(throwing: error)
-                }
-                
-                continuation.resume(returning: (data, response))
-            }
-            
-            task.resume()
-        }
-    }
-    
-    func data(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: urlRequest) { data, response, error in
-                guard let data = data, let response = response else {
-                    let error = error ?? URLError(.badServerResponse)
-                    return continuation.resume(throwing: error)
-                }
-                
-                continuation.resume(returning: (data, response))
-            }
-            
-            task.resume()
-        }
-    }
-}
---
+++ Sources/SolanaSwift/Extensions/Concurrency+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chung Tran on 13/04/2022.
+//
+
+import Foundation
+
+@available(macOS, deprecated: 12.0, message: "Use the built-in API instead")
+extension URLSession {
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+                
+                continuation.resume(returning: (data, response))
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func data(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: urlRequest) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+                
+                continuation.resume(returning: (data, response))
+            }
+            
+            task.resume()
+        }
+    }
+}
