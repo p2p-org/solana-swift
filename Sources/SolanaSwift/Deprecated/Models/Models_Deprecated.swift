56ca871e1ecb7f410bc84fd35353b07663bc45b5
Alexey Sidorov <ody344@gmail.com>
2022-04-22 13:08:30 +0300

    Optimized git status

--- Sources/SolanaSwift/Deprecated/Models/Models_Deprecated.swift
+++ Sources/SolanaSwift/Deprecated/Models/Models_Deprecated.swift
@@ -10,86 +10,36 @@ public extension SolanaSDK_Deprecated {
     typealias BufferInfo = SolanaSwift.BufferInfo
     typealias SignatureInfo = SolanaSwift.SignatureInfo
     typealias Response = SolanaSwift.Response
-    
-}
-
-// Network
-public extension SolanaSDK_Deprecated {
+    // Network
     typealias Network = SolanaSwift.Network
-}
-
-// Pool
-public extension SolanaSDK_Deprecated {
+    // Pool
     typealias Pool = SolanaSwift.Pool
-}
-
-// PreparedTransaction
-public extension SolanaSDK_Deprecated {
+    // PreparedTransaction
     typealias PreparedTransaction = SolanaSwift.PreparedTransaction
-}
-
-// PublicKey
-public extension SolanaSDK_Deprecated {
+    // PublicKey
     typealias PublicKey = SolanaSwift.PublicKey
-}
-
-public extension SolanaSDK_Deprecated {
     typealias TransactionInstruction = SolanaSwift.TransactionInstruction
-}
-
-public extension SolanaSDK_Deprecated {
     typealias Wallet = SolanaSwift.Wallet
-}
 
-public extension SolanaSDK_Deprecated {
     typealias FeeAmount = SolanaSwift.FeeAmount
-}
-
-public extension SolanaSDK_Deprecated {
     typealias DerivablePath = SolanaSwift.DerivablePath
-}
-
-public extension SolanaSDK_Deprecated {
     typealias ConfirmedTransaction = SolanaSwift.ConfirmedTransaction
     typealias ParsedInstruction = SolanaSwift.ParsedInstruction
-}
-
-public extension SolanaSDK_Deprecated {
     typealias APIEndPoint = SolanaSwift.APIEndPoint
-}
-
-public extension SolanaSDK_Deprecated {
     typealias Account = SolanaSwift.Account
-}
 
-// MARK: -  Solana Token
-public extension SolanaSDK_Deprecated {
+    // MARK: -  Solana Token
     typealias TokenTag = SolanaSwift.TokenTag
     typealias WrappingToken = SolanaSwift.WrappingToken
     typealias Token = SolanaSwift.Token
     typealias TokenExtensions = SolanaSwift.TokenExtensions
-}
-
-// MARK: - SendingTransaction
-
-public extension SolanaSDK_Deprecated {
+    
+    // MARK: - SendingTransaction
     typealias Transaction = SolanaSwift.Transaction
-}
 
-// MARK: - BufferLayout
-
-public extension SolanaSDK_Deprecated {
+    // MARK: - BufferLayout
     typealias AccountInfo = SolanaSwift.AccountInfo
-}
-
-public extension SolanaSDK_Deprecated {
     typealias EmptyInfo = SolanaSwift.EmptyInfo
-}
-
-public extension SolanaSDK_Deprecated {
     typealias Mint = SolanaSwift.Mint
-}
-
-public extension SolanaSDK_Deprecated {
     typealias TokenSwapInfo = SolanaSwift.TokenSwapInfo
 }
