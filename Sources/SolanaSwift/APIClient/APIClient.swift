0123ab2192ade82e95d088b18cf57b6c38448742
Alexey Sidorov <ody344@gmail.com>
2022-04-28 18:19:34 +0300

    Added new methods

--- Sources/SolanaSwift/APIClient/APIClient.swift
+++ Sources/SolanaSwift/APIClient/APIClient.swift
@@ -15,6 +15,11 @@ public protocol SolanaAPIClient {
     func getConfirmedBlock(slot: UInt64, encoding: String) async throws -> ConfirmedBlock
     func getConfirmedSignaturesForAddress(account: String, startSlot: UInt64, endSlot: UInt64) async throws -> [String]
     func getEpochInfo(commitment: Commitment?) async throws -> EpochInfo
+    func getFees(commitment: Commitment?) async throws -> Fee
+    func getMinimumBalanceForRentExemption(dataLength: UInt64, commitment: Commitment?) async throws -> UInt64
+    func getSignatureStatuses(signatures: [String], configs: RequestConfiguration?) async throws -> [SignatureStatus?]
+    func getSignatureStatus(signature: String, configs: RequestConfiguration?) async throws -> SignatureStatus
+    func getTokenAccountBalance(pubkey: String, commitment: Commitment?) async throws -> TokenAccountBalance
 
     // Requests
     func request<Entity: Decodable>(with request: RequestEncoder.RequestType) async throws -> AnyResponse<Entity>
@@ -41,21 +46,11 @@ extension SolanaAPIClient {
     }
     
     public func getBlockHeight() async throws -> UInt64 {
-        let req = RequestEncoder.RequestType(method: "getBlockHeight", params: [])
-        let response: AnyResponse<UInt64> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getBlockHeight", params: [])
     }
     
     public func getConfirmedBlocksWithLimit(startSlot: UInt64, limit: UInt64) async throws -> [UInt64] {
-        let req = RequestEncoder.RequestType(method: "getConfirmedBlocksWithLimit", params: [startSlot, limit])
-        let response: AnyResponse<[UInt64]> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getConfirmedBlocksWithLimit", params: [startSlot, limit])
     }
     
     public func getBalance(account: String, commitment: Commitment? = nil) async throws -> UInt64 {
@@ -68,12 +63,7 @@ extension SolanaAPIClient {
     }
     
     public func getBlockCommitment(block: UInt64) async throws -> BlockCommitment {
-        let req = RequestEncoder.RequestType(method: "getBlockCommitment", params: [block])
-        let response: AnyResponse<BlockCommitment> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getBlockCommitment", params: [block])
     }
     
     public func getBlockTime(block: UInt64) async throws -> Date {
@@ -84,48 +74,74 @@ extension SolanaAPIClient {
     }
     
     public func getClusterNodes() async throws -> [ClusterNodes] {
-        let req = RequestEncoder.RequestType(method: "getClusterNodes", params: [])
-        let response: AnyResponse<[ClusterNodes]> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getClusterNodes", params: [])
     }
     
     public func getConfirmedBlock(slot: UInt64, encoding: String) async throws -> ConfirmedBlock {
-        let req = RequestEncoder.RequestType(method: "getConfirmedBlock", params: [slot, encoding])
-        let response: AnyResponse<ConfirmedBlock> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getConfirmedBlock", params: [slot, encoding])
     }
     
     public func getConfirmedSignaturesForAddress(account: String, startSlot: UInt64, endSlot: UInt64) async throws -> [String] {
-        let req = RequestEncoder.RequestType(method: "getConfirmedSignaturesForAddress", params: [account, startSlot, endSlot])
-        let response: AnyResponse<[String]> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
+        return try await self.get(method: "getConfirmedSignaturesForAddress", params: [account, startSlot, endSlot])
     }
     
     public func getTransaction(transactionSignature: String) async throws -> TransactionInfo {
-        let req = RequestEncoder.RequestType(method: "getTransaction", params: [transactionSignature, "jsonParsed"])
-        let response: AnyResponse<TransactionInfo> = try await request(with: req)
+        return try await self.get(method: "getTransaction", params: [transactionSignature, "jsonParsed"])
+    }
+    
+    public func getEpochInfo(commitment: Commitment? = nil) async throws -> EpochInfo {
+        return try await self.get(method: "getEpochInfo", params: [RequestConfiguration(commitment: commitment)])
+    }
+    
+    public func getFees(commitment: Commitment? = nil) async throws -> Fee {
+        return try await self.get(method: "getFees", params: [RequestConfiguration(commitment: commitment)])
+    }
+    
+    public func getMinimumBalanceForRentExemption(dataLength: UInt64, commitment: Commitment? = "recent") async throws -> UInt64 {
+        return try await self.get(method: "getMinimumBalanceForRentExemption", params: [dataLength, RequestConfiguration(commitment: commitment)])
+    }
+    
+    public func getMinimumBalanceForRentExemption(span: UInt64) async throws -> UInt64 {
+        return try await self.getMinimumBalanceForRentExemption(dataLength: span)
+    }
+    
+    public func getRecentBlockhash(commitment: Commitment? = nil) async throws -> String {
+        let result: Rpc<Fee> = try await self.get(method: "getRecentBlockhash", params: [RequestConfiguration(commitment: commitment)])
+        guard let blockhash = result.value.blockhash else {
+            throw SolanaSDK.Error.other("Blockhash not found")
+        }
+        return blockhash
+    }
+    
+    public func getSignatureStatuses(signatures: [String], configs: RequestConfiguration? = nil) async throws -> [SignatureStatus?] {
+        let result: Rpc<[SignatureStatus?]> = try await self.get(method: "getSignatureStatuses", params: [signatures, configs])
+        return result.value
+    }
+    
+    public func getSignatureStatus(signature: String, configs: RequestConfiguration? = nil) async throws -> SignatureStatus {
+        guard let result = try await getSignatureStatuses(signatures: [signature]).first else {
+            throw APIClientError.cantDecodeResponse
+        }
+        return try result ?! APIClientError.cantDecodeResponse
+    }
+    
+    public func getTokenAccountBalance(pubkey: String, commitment: Commitment? = nil) async throws -> TokenAccountBalance {
+        let result: Rpc<TokenAccountBalance> = try await self.get(method: "getTokenAccountBalance", params: [pubkey, RequestConfiguration(commitment: commitment)])
+        if UInt64(result.value.amount) == nil {
+            throw SolanaSDK.Error.couldNotRetrieveAccountInfo
+        }
+        return result.value
+    }
+
+    
+    private func get<Entity: Decodable>(method: String, params: [Encodable]) async throws -> Entity {
+        let req = RequestEncoder.RequestType(method: method, params: params)
+        let response: AnyResponse<Entity> = try await request(with: req)
         guard let result = response.result else {
             throw APIClientError.cantDecodeResponse
         }
         return result
     }
     
-    public func getEpochInfo(commitment: Commitment? = nil) async throws -> EpochInfo {
-        let req = RequestEncoder.RequestType(method: "getEpochInfo", params: [RequestConfiguration(commitment: commitment)])
-        let response: AnyResponse<EpochInfo> = try await request(with: req)
-        guard let result = response.result else {
-            throw APIClientError.cantDecodeResponse
-        }
-        return result
-    }
 
 }
