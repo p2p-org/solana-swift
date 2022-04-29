392bed776b629b82bca36935b35f7b759d4c6913
Giang Long Tran <trangianglong4003@gmail.com>
2022-04-29 15:51:16 +0200

    fix

--- Sources/SolanaSwift/Model/SolanaSDK+Models.swift
+++ Sources/SolanaSwift/Model/SolanaSDK+Models.swift
@@ -216,8 +216,8 @@ public struct ErrorDetail: Codable {
 }
 
 public struct InnerInstruction: Decodable {
-    let index: UInt32
-    let instructions: [ParsedInstruction]
+    public let index: UInt32
+    public let instructions: [ParsedInstruction]
 }
 public struct TokenBalance: Decodable {
     let accountIndex: UInt64
