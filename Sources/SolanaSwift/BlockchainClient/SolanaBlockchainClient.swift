b2cb3bf41b92f5f04d1a243af8163ee697ee242d
Chung Tran <bigearsenal@gmail.com>
2022-05-04 18:02:42 +0700

    refactor: rename Protocol, Impl

---
+++ Sources/SolanaSwift/BlockchainClient/SolanaBlockchainClient.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chung Tran on 04/05/2022.
+//
+
+import Foundation
+
+public protocol SolanaBlockchainClient: AnyObject {
+    /// Prepare a transaction base on its instructions
+    /// - Parameters:
+    ///   - instructions: instructions of the transaction
+    ///   - signers: the signers
+    ///   - feePayer: the feePayer, usually is the first signer
+    ///   - accountsCreationFee: estimated account creation fee
+    ///   - recentBlockhash: recentBlockhash, can be fetched lately when the value is nil
+    ///   - lamportsPerSignature: lamportsPerSignature, can be automatically fetched when the value is nil
+    /// - Returns: information of a prepared transaction
+    func prepareTransaction(
+        instructions: [TransactionInstruction],
+        signers: [Account],
+        feePayer: PublicKey,
+        accountsCreationFee: Lamports,
+        recentBlockhash: String?,
+        lamportsPerSignature: Lamports?
+    ) async throws -> PreparedTransaction
+    
+    /// Serialize PreparedTransaction for sending
+    /// - Parameter preparedTransaction: a prepared transaction
+    /// - Returns: Serialized transaction which is ready to be sent
+    func serialize(
+        preparedTransaction: PreparedTransaction
+    ) throws -> String
+}
+
+extension SolanaBlockchainClient {
+    func serialize(
+        preparedTransaction: PreparedTransaction
+    ) throws -> String {
+        try preparedTransaction.serialize()
+    }
+}
