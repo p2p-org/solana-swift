5f7c2908494a2bf221511c8adf623c358448034d
Chung Tran <bigearsenal@gmail.com>
2022-05-06 17:47:34 +0700

    feat: ObserveTransactionStatusTest

--- Tests/SolanaSwiftTests/APIClient/ObserveTransactionStatusTests.swift
+++ Tests/SolanaSwiftTests/APIClient/ObserveTransactionStatusTests.swift
@@ -13,19 +13,28 @@ class ObserveTransactionStatusTests: XCTestCase {
     
     func testObservingTransactionStatus() async throws {
         let mock = NetworkManagerMock([
-            .success(#""#),
+            .success(mockResponse(confirmations: 0, confirmationStatus: "processed")),
             .failure(CustomError.unknownNetworkError),
-            .success(#""#),
+            .success(mockResponse(confirmations: 1, confirmationStatus: "confirmed")),
             .failure(CustomError.unknownNetworkError),
             .failure(SolanaError.unknown),
-            .success(#""#),
-            .success(#""#),
-            .success(#""#),
-            .success(#""#)
+            .success(mockResponse(confirmations: 5, confirmationStatus: "confirmed")),
+            .success(mockResponse(confirmations: 10, confirmationStatus: "confirmed")),
+            .failure(SolanaError.unknown),
+            .success(mockResponse(confirmations: nil, confirmationStatus: "finalized")),
         ])
         let apiClient = SolanaSwift.JSONRPCAPIClient(endpoint: endpoint, networkManager: mock)
-        let result = try! await apiClient.getBlockHeight()
-        XCTAssertEqual(result, 119396901)
+        for try await status in apiClient.observeSignatureStatus(signature: "jaiojsdfoijvaij") {
+            print(status)
+        }
+//        let result = try! await apiClient.getBlockHeight()
+//        XCTAssertEqual(result, 119396901)
+    }
+    
+    // MARK: - Helpers
+    
+    func mockResponse(confirmations: Int?, confirmationStatus: String) -> String {
+        #"{"jsonrpc":"2.0","result":{"context":{"slot":82},"value":[{"slot":72,"confirmations":\#(confirmations != nil ? "\(confirmations!)": "null"),"err":null,"status":{"Ok":null},"confirmationStatus":"\#(confirmationStatus)"},null]},"id":1}"#
     }
     
     class NetworkManagerMock: NetworkManager {
