69ec406174c199a08e7386d9a37c07449aea0099
Chung Tran <bigearsenal@gmail.com>
2022-05-06 09:22:23 +0700

    feat: move model outside

--- Sources/SolanaSwift/Deprecated/SolanaSDK+Actions/Swap/SolanaSDK+Swap.swift
+++ Sources/SolanaSwift/Deprecated/SolanaSDK+Actions/Swap/SolanaSDK+Swap.swift
@@ -9,44 +9,8 @@ import Foundation
 import RxSwift
 
 extension SolanaSDK {
-    public struct SwapResponse {
-        public let transactionId: String
-        public let newWalletPubkey: String?
-        
-        public init(transactionId: String, newWalletPubkey: String?) {
-            self.transactionId = transactionId
-            self.newWalletPubkey = newWalletPubkey
-        }
-    }
-    
-    public struct AccountInstructions {
-        public init(
-            account: PublicKey,
-            instructions: [SolanaSDK.TransactionInstruction] = [],
-            cleanupInstructions: [SolanaSDK.TransactionInstruction] = [],
-            signers: [Account] = [],
-            newWalletPubkey: String? = nil,
-            secretKey: Data? = nil
-        ) {
-            self.account = account
-            self.instructions = instructions
-            self.cleanupInstructions = cleanupInstructions
-            self.signers = signers
-            self.newWalletPubkey = newWalletPubkey
-            self.secretKey = secretKey
-        }
-        
-        public let account: PublicKey
-        public var instructions: [TransactionInstruction]
-        public var cleanupInstructions: [TransactionInstruction]
-        public let signers: [Account]
-        
-        // additionally return new wallet address
-        fileprivate private(set) var newWalletPubkey: String?
-        
-        // additionally return newAccount's secretkey
-        fileprivate private(set) var secretKey: Data?
-    }
+    public typealias SwapResponse = SolanaSwift.SwapResponse
+    public typealias AccountInstructions = SolanaSwift.AccountInstructions
     
     // MARK: - Account and instructions
     public func prepareSourceAccountAndInstructions(
