f5617fc80e9e2767c02f58689fe9a90b3e2e7bef
Chung Tran <bigearsenal@gmail.com>
2022-05-12 19:10:56 +0700

    fix: BorshSerializable

--- Sources/SolanaSwift/Models/BufferLayout/BufferLayout.swift
+++ Sources/SolanaSwift/Models/BufferLayout/BufferLayout.swift
@@ -7,28 +7,33 @@ public protocol BufferLayout: Codable, BorshCodable {
     static var BUFFER_LENGTH: UInt64 { get }
 }
 
-public struct Buffer<T: BufferLayout>: Codable {
-    public let value: T?
-
+extension BufferLayout {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-
-        // decode parsedJSON
-        if let parsedData = try? container.decode(T.self) {
-            value = parsedData
-            return
-        }
-
+        
+//        // decode parsedJSON
+//        if let parsedData = try? container.decode(Self.self) {
+//            self = parsedData
+//            return
+//        }
+        
         // Unable to get parsed data, fallback to decoding base64
         let stringData = (try? container.decode([String].self).first) ?? (try? container.decode(String.self))
-        guard let string = stringData, let data = Data(base64Encoded: string)?.bytes,
-              data.count >= T.BUFFER_LENGTH
+        guard let string = stringData,
+              let data = Data(base64Encoded: string)
         else {
-            value = nil
-            return
+            throw SolanaError.couldNotRetrieveAccountInfo
         }
-
-        var reader = BinaryReader(bytes: data)
-        value = try T.init(from: &reader)
+        
+        if string.isEmpty && !(Self.self == EmptyInfo.self) {
+            throw SolanaError.couldNotRetrieveAccountInfo
+        }
+        
+        do {
+            var reader = BinaryReader(bytes: data.bytes)
+            try self.init(from: &reader)
+        } catch {
+            throw SolanaError.couldNotRetrieveAccountInfo
+        }
     }
 }
