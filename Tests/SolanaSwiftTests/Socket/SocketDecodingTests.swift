8c8b1440e6524e5af62ce3c0ee2003c32928ec52
Chung Tran <bigearsenal@gmail.com>
2022-05-16 12:43:09 +0700

    feat: add testing events

--- Tests/SolanaSwiftTests/Socket/SocketDecodingTests.swift
+++ Tests/SolanaSwiftTests/Socket/SocketDecodingTests.swift
@@ -4,7 +4,7 @@ import SolanaSwift
 class SocketDecodingTests: XCTestCase {
     
     func testDecodingSocketSubscription() throws {
-        let string = #"{"jsonrpc":"2.0","result":22529999,"id":"ADFB8971-4473-4B16-A8BC-63EFD2F1FC8E"}"#
+        let string = SocketTestsHelper.emittingEvents["subscriptionNotification"]!
         let result = try JSONDecoder().decode(SocketSubscriptionResponse.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.id, "ADFB8971-4473-4B16-A8BC-63EFD2F1FC8E")
@@ -12,7 +12,7 @@ class SocketDecodingTests: XCTestCase {
     }
     
     func testDecodingSocketUnsubscription() throws {
-        let string = #"{ "jsonrpc": "2.0", "result": true, "id": "ADFB8971-4473-4B16-A8BC-63EFD2F1FC8E" }"#
+        let string = SocketTestsHelper.emittingEvents["unsubscriptionNotification"]!
         let result = try JSONDecoder().decode(SocketUnsubscriptionResponse.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.id, "ADFB8971-4473-4B16-A8BC-63EFD2F1FC8E")
@@ -20,7 +20,7 @@ class SocketDecodingTests: XCTestCase {
     }
     
     func testDecodingSOLAccountNotification() throws {
-        let string = #"{"jsonrpc":"2.0","method":"accountNotification","params":{"result":{"context":{"slot":80221533},"value":{"data":["","base64"],"executable":false,"lamports":41083620,"owner":"11111111111111111111111111111111","rentEpoch":185}},"subscription":46133}}"#
+        let string = SocketTestsHelper.emittingEvents["accountNotification#Native"]!
         let result = try JSONDecoder().decode(SocketNativeAccountNotification.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.method, "accountNotification")
@@ -28,7 +28,7 @@ class SocketDecodingTests: XCTestCase {
     }
     
     func testDecodingProgramNotification() throws {
-        let string = #"{"jsonrpc":"2.0","method":"programNotification","params":{"result":{"context":{"slot":5208469},"value":{"pubkey":"H4vnBqifaSACnKa7acsxstsY1iV1bvJNxsCY7enrd1hq","account":{"data":["11116bv5nS2h3y12kD1yUKeMZvGcKLSjQgX6BeV7u1FrjeJcKfsHPXHRDEHrBesJhZyqnnq9qJeUuF7WHxiuLuL5twc38w2TXNLxnDbjmuR","base58"],"executable":false,"lamports":33594,"owner":"11111111111111111111111111111111","rentEpoch":636}}},"subscription":24040}}"#
+        let string = SocketTestsHelper.emittingEvents["programNotification"]!
         let result = try JSONDecoder().decode(SocketProgramAccountNotification.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.method, "programNotification")
@@ -36,8 +36,7 @@ class SocketDecodingTests: XCTestCase {
     }
     
     func testDecodingTokenAccountNotification() throws {
-        let string = #"{"jsonrpc":"2.0","method":"accountNotification","params":{"result":{"context":{"slot":80216037},"value":{"data":{"parsed":{"info":{"isNative":false,"mint":"kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6","owner":"6QuXb6mB6WmRASP2y8AavXh6aabBXEH5ZzrSH5xRrgSm","state":"initialized","tokenAmount":{"amount":"390000101","decimals":5,"uiAmount":3900.00101,"uiAmountString":"3900.00101"}},"type":"account"},"program":"spl-token","space":165},"executable":false,"lamports":2039280,"owner":"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA","rentEpoch":185}},"subscription":42765}}"#
-        
+        let string = SocketTestsHelper.emittingEvents["accountNotification#Token"]!
         let result = try JSONDecoder().decode(SocketTokenAccountNotification.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.method, "accountNotification")
@@ -45,8 +44,7 @@ class SocketDecodingTests: XCTestCase {
     }
     
     func testDecodingSignatureNotification() throws {
-        let string = #"{"jsonrpc":"2.0","method":"signatureNotification","params":{"result":{"context":{"slot":80768508},"value":{"err":null}},"subscription":43601}}"#
-        
+        let string = SocketTestsHelper.emittingEvents["signatureNotification"]!
         let result = try JSONDecoder().decode(SocketSignatureNotification.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.method, "signatureNotification")
@@ -54,8 +52,7 @@ class SocketDecodingTests: XCTestCase {
     }
 
     func testDecodingLogsNotification() throws {
-        let string = #"{"jsonrpc":"2.0","method":"logsNotification","params":{"result":{"context":{"slot":5208469},"value":{"signature":"5h6xBEauJ3PK6SWCZ1PGjBvj8vDdWG3KpwATGy1ARAXFSDwt8GFXM7W5Ncn16wmqokgpiKRLuS83KUxyZyv2sUYv","err":null,"logs":["BPF program 83astBRguLMdt2h5U1Tpdq5tjFoJ6noeGwaY3mDLVcri success"]}},"subscription":24040}}"#
-        
+        let string = SocketTestsHelper.emittingEvents["logsNotification"]!
         let result = try JSONDecoder().decode(SocketLogsNotification.self, from: string.data(using: .utf8)!)
         
         XCTAssertEqual(result.method, "logsNotification")
