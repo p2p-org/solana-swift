b7c0d9827e4ce73976a82df20f6d211685709962
Chung Tran <bigearsenal@gmail.com>
2022-05-16 09:31:41 +0700

    feat: some fixes

---
+++ Sources/SolanaSwift/Socket/SocketModels.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public enum SocketMethod: String {
+    case accountNotification
+    case accountSubscribe
+    case accountUnsubscribe
+
+    case signatureNotification
+    case signatureSubscribe
+    case signatureUnsubscribe
+
+    case logsSubscribe
+    case logsNotification
+    case logsUnsubscribe
+
+    case programSubscribe
+    case programNotification
+    case programUnsubscribe
+
+    case slotSubscribe
+    case slotNotification
+    case slotUnsubscribe
+}
+
+struct SocketSubscription: Decodable {
+    let jsonrpc: String
+    let id: String
+    let result: UInt64
+}
+
+public struct SignatureNotification: Decodable {
+    let err: ResponseError?
+}
+
+public struct LogsNotification: Decodable {
+    let signature: String
+    let logs: [String]
+    let err: ResponseError?
+}
