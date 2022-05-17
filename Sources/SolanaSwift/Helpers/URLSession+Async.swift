1cd67b8f8d76c6ac6511f42b7c0e4d03dafb7c66
Chung Tran <bigearsenal@gmail.com>
2022-05-17 14:11:23 +0700

    feat: method data

--- Sources/SolanaSwift/Helpers/URLSession+Async.swift
+++ Sources/SolanaSwift/Helpers/URLSession+Async.swift
@@ -3,7 +3,7 @@ import Foundation
 @available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
 @available(macOS, deprecated: 12.0, message: "Use the built-in API instead")
 extension URLSession {
-    func data(from url: URL) async throws -> (Data, URLResponse) {
+    func data(for url: URL) async throws -> (Data, URLResponse) {
         try await withCheckedThrowingContinuation { continuation in
             let task = self.dataTask(with: url) { data, response, error in
                 guard let data = data, let response = response else {
@@ -18,7 +18,7 @@ extension URLSession {
         }
     }
     
-    func data(from urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+    func data(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
         try await withCheckedThrowingContinuation { continuation in
             let task = self.dataTask(with: urlRequest) { data, response, error in
                 guard let data = data, let response = response else {
--- Sources/SolanaSwift/Helpers/URLSession+NetworkManager.swift
+++ Sources/SolanaSwift/Helpers/URLSession+NetworkManager.swift
@@ -4,11 +4,7 @@ extension URLSession: NetworkManager {
     
     public func requestData(request: URLRequest) async throws -> Data {
         let (data, _): (Data, URLResponse)
-        if #available(iOS 15.0, macOS 12.0, *) {
-            (data, _) = try await self.data(for: request)
-        } else {
-            (data, _) = try await self.data(from: request)
-        }
+        (data, _) = try await self.data(for: request)
         return data
     }
 }
