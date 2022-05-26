4beabb7038ffc7693543b3709410b50f6d6ec516
Chung Tran <bigearsenal@gmail.com>
2022-05-26 08:44:03 +0700

    Revert "feat: update dependencies"
    This reverts commit 8ea04fc903585297b2ed188b1f9ab5d9fae32cd5.

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
-        .package(name: "TweetNacl", url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", from: "1.1.0"),
+        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
+        .package(name: "TweetNacl", url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", from: "1.0.2"),
 
         .package(name: "Task_retrying", url: "https://github.com/bigearsenal/task-retrying-swift.git", from: "1.0.2"),
         .package(name: "LoggerSwift", url: "https://github.com/bigearsenal/loggerswift.git", from: "1.0.2"),
