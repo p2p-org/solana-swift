9d62b5a95c53115bc07ddc995fcb9173c551735c
Chung Tran <bigearsenal@gmail.com>
2022-06-03 19:27:34 +0700

    feat: remove asynchronous creating account function

--- Sources/SolanaSwift/Models/Account.swift
+++ Sources/SolanaSwift/Models/Account.swift
@@ -12,53 +12,6 @@ public struct Account: Codable, Hashable {
         self.secretKey = secretKey
     }
 
-    /// Create account with seed phrase
-    /// - Parameters:
-    ///   - phrase: secret phrase for an account, leave it empty for new account
-    ///   - network: network in which account should be created
-    /// - Throws: Error if the derivation is not successful
-    @available(*, deprecated, message: "This function is deprecated, use init async throws instead")
-    public init(phrase: [String] = [], network: Network, derivablePath: DerivablePath? = nil) throws {
-        let mnemonic: Mnemonic
-        var phrase = phrase.filter { !$0.isEmpty }
-        if !phrase.isEmpty {
-            mnemonic = try Mnemonic(phrase: phrase)
-        } else {
-            // change from 12-words to 24-words (128 to 256)
-            mnemonic = Mnemonic()
-            phrase = mnemonic.phrase
-        }
-        self.phrase = phrase
-
-        var derivablePath = derivablePath
-        if derivablePath == nil {
-            if phrase.count == 12 {
-                derivablePath = .init(type: .deprecated, walletIndex: 0, accountIndex: 0)
-            } else {
-                derivablePath = .default
-            }
-        }
-
-        switch derivablePath!.type {
-        case .deprecated:
-            let keychain = try Keychain(seedString: phrase.joined(separator: " "), network: network.cluster)
-            guard let seed = try keychain?.derivedKeychain(at: derivablePath!.rawValue).privateKey else {
-                throw SolanaError.other("Could not derivate private key")
-            }
-
-            let keys = try NaclSign.KeyPair.keyPair(fromSeed: seed)
-
-            publicKey = try PublicKey(data: keys.publicKey)
-            secretKey = keys.secretKey
-        default:
-            let keys = try Ed25519HDKey.derivePath(derivablePath!.rawValue, seed: mnemonic.seed.toHexString()).get()
-            let keyPair = try NaclSign.KeyPair.keyPair(fromSeed: keys.key)
-            let newKey = try PublicKey(data: keyPair.publicKey)
-            publicKey = newKey
-            secretKey = keyPair.secretKey
-        }
-    }
-
     public init(secretKey: Data) throws {
         let keys = try NaclSign.KeyPair.keyPair(fromSecretKey: secretKey)
         publicKey = try PublicKey(data: keys.publicKey)
