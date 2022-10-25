53f1e39aeee2fdfd3ad44de6f6ee369118700921
Chung Tran <bigearsenal@gmail.com>
2022-10-25 11:42:57 +0700

    feat: rename to url

--- Sources/SolanaSwift/TokensRepository/TokensListParser.swift
+++ Sources/SolanaSwift/TokensRepository/TokensListParser.swift
@@ -12,10 +12,10 @@ public class TokensListParser {
     private let tokenListURL: URL?
 
     public init(
-        tokenListURL: String = "https://raw.githubusercontent.com/bigearsenal/solana-token-list/main/src/tokens/solana.tokenlist.json",
+        url: String = "https://raw.githubusercontent.com/bigearsenal/solana-token-list/main/src/tokens/solana.tokenlist.json",
         networkManager: NetworkManager = URLSession.shared
     ) {
-        self.tokenListURL = URL(string: tokenListURL)
+        self.tokenListURL = URL(string: url)
         self.networkManager = networkManager
     }
 
