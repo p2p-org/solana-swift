539d9ddfcd4989fd4aedb0f06ab736355fe2d539
Chung Tran <bigearsenal@gmail.com>
2023-03-13 21:05:53 +0700

    feat: add SendTransactionTests

---
+++ Tests/SolanaSwiftUnitTests/APIClient/NetworkManagerMock/NetworkManagerMock.swift
@@ -0,0 +1,21 @@
+import SolanaSwift
+import Foundation
+
+class NetworkManagerMock: NetworkManager {
+    private let json: String
+    init(_ json: String) {
+        self.json = json
+    }
+
+    func requestData(request _: URLRequest) async throws -> Data {
+        let str = json.data(using: .utf8)!
+        return str
+    }
+}
+
+extension NetworkManagerMock {
+    func requestData(request _: URLRequest, res: String) async throws -> Data {
+        let str = res.data(using: .utf8)!
+        return str
+    }
+}
