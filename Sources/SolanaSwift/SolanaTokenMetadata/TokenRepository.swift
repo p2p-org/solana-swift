bdafc041a3b0ab8fbf7084ea81aa3cbb62f0c42d
Tran Giang Long <trangianglong4003@gmail.com>
2023-06-27 13:26:32 +0700

    revert

--- Sources/SolanaSwift/SolanaTokenMetadata/TokenRepository.swift
+++ Sources/SolanaSwift/SolanaTokenMetadata/TokenRepository.swift
@@ -15,21 +15,4 @@ public extension TokenRepository {
     func get(address: PublicKey) async throws -> TokenMetadata? {
         try await get(address: address.base58EncodedString)
     }
-
-    func safeGet(address: String?) async throws -> TokenMetadata {
-        if let address {
-            return try await get(address: address)
-                ?? .unsupported(mint: address, decimals: 1, symbol: "", supply: nil)
-        } else {
-            return .unsupported(mint: "", decimals: 1, symbol: "", supply: nil)
-        }
-    }
-
-    func safeGet(address: PublicKey?) async throws -> TokenMetadata {
-        if let address {
-            return try await safeGet(address: address.base58EncodedString)
-        } else {
-            return .unsupported(mint: "", decimals: 1, symbol: "", supply: nil)
-        }
-    }
 }
