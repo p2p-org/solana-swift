f006281a095d27a2587f72896d98df5f439b5d4b
Chung Tran <bigearsenal@gmail.com>
2024-01-05 15:42:51 +0700

    feat: add tests

--- Tests/SolanaSwiftUnitTests/APIClient/APIClientTests.swift
+++ Tests/SolanaSwiftUnitTests/APIClient/APIClientTests.swift
@@ -281,9 +281,23 @@ class APIClientTests: XCTestCase {
         let result = try await apiClient.getTokenAccountsByOwner(
             pubkey: "fjohsw9j8aBJaEE5ddzHk3AgMjdbQSXrMoPGrepNHrB",
             params: .init(mint: nil, programId: TokenProgram.id.base58EncodedString),
-            configs: .init(encoding: "base64")
+            configs: .init(encoding: "base64"),
+            decodingTo: Token2022AccountState.self
         )
         XCTAssertEqual(result.first?.account.owner, TokenProgram.id.base58EncodedString)
+    }
+
+    func testGetToken2022AccountsByOwner() async throws {
+        let mock = NetworkManagerMock(NetworkManagerMockJSON["getToken2022AccountsByOwner"]!)
+        let apiClient = JSONRPCAPIClient(endpoint: endpoint, networkManager: mock)
+        let result = try await apiClient.getTokenAccountsByOwner(
+            pubkey: "fjohsw9j8aBJaEE5ddzHk3AgMjdbQSXrMoPGrepNHrB",
+            params: .init(mint: nil, programId: TokenProgram.id.base58EncodedString),
+            configs: .init(encoding: "base64"),
+            decodingTo: Token2022AccountState.self
+        )
+        XCTAssertEqual(result.first?.account.owner, Token2022Program.id.base58EncodedString)
+        XCTAssertEqual(result.first?.pubkey, "43W7QvyKr5hJhFRhvteb7VbsLdwGQG3VZ2fRYVcw5yFN")
     }
 
     func testSendTransactionError1() async throws {
@@ -368,6 +382,8 @@ private var NetworkManagerMockJSON = [
     "getSignatureStatuses": #"{"jsonrpc":"2.0","result":{"context":{"slot":82},"value":[{"slot":72,"confirmations":10,"err":null,"status":{"Ok":null},"confirmationStatus":"confirmed"},null]},"id":1}"#,
     "getSignatureForAddress": "{\"jsonrpc\":\"2.0\",\"result\":[{\"blockTime\":1652351736,\"confirmationStatus\":\"finalized\",\"err\":null,\"memo\":null,\"signature\":\"31vx5MqPX1cxzfwGEWjaHuHnmSo9vwrtwBNXyTJjxtfbzkzqWr4sY8JE5Mq5ZZK7aokps9UjhHcNuJTF82FP2ekM\",\"slot\":133510309}],\"id\":\"6D72B578-401C-449E-A89F-2E31EA441A47\"}\n",
     "getTokenAccountsByOwner": "{\"jsonrpc\":\"2.0\",\"result\":{\"context\":{\"slot\":134133059},\"value\":[{\"account\":{\"data\":[\"KLrvuAuq+8gDEGl28m80PrYteWuPlqjGuBpCW5rA84gJ7HiGa7fztefqNjU2MSBOZ3HPlRmb0eAXj0bEanmyfAoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"base64\"],\"executable\":false,\"lamports\":2039280,\"owner\":\"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA\",\"rentEpoch\":309},\"pubkey\":\"9nuVPk3KR7oUXmDsHL7irue2Nxaj3ejvuBXoaEcXMmN7\"},{\"account\":{\"data\":[\"ppdSk884LShYnHoHm7XiDlZ28iJVm9BHPgrAEfxU44AJ7HiGa7fztefqNjU2MSBOZ3HPlRmb0eAXj0bEanmyfAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"base64\"],\"executable\":false,\"lamports\":2039280,\"owner\":\"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA\",\"rentEpoch\":309},\"pubkey\":\"9bNJ7AF8w1Ms4BsqpqbUPZ16vCSePYJpgSBUTRqd8ph4\"}]},\"id\":\"0891812F-1F8B-4927-80F7-C7C1C1D990B3\"}\n",
+    "getToken2022AccountsByOwner":
+        #"{"jsonrpc":"2.0","result":{"context":{"apiVersion":"1.17.14","slot":239989261},"value":[{"account":{"data":["qDijZLhcKuVLVBmL6Ve9S9DvSU7kn1XtkCnOtnDXGjA1zKFCx20D2kF7X/jEMh09sgYqyBraJk1DWsFwaUfQkCtqbAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgcAAAACAAgAAAAAAAAAAAA=","base64"],"executable":false,"lamports":2157600,"owner":"TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb","rentEpoch":0,"space":182},"pubkey":"43W7QvyKr5hJhFRhvteb7VbsLdwGQG3VZ2fRYVcw5yFN"}]},"id":1}"#,
     "sendTransactionError1": #"{"jsonrpc":"2.0","error":{"code":-32003,"message":"Transaction precompile verification failure InvalidAccountIndex"},"id":"7DEDE6E5-95E7-4866-BFC0-B4C10A76B457"}"#,
     "getTokenAccountBalance": #"{"jsonrpc":"2.0","result":{"context":{"slot":135942588},"value":{"amount":"491717631607","decimals":9,"uiAmount":491.717631607,"uiAmountString":"491.717631607"}},"id":"3D9E7B6E-B48D-40EF-B656-EA3054227CCD"}"#,
     "getHealth": #"{ "jsonrpc": "2.0", "result": "ok", "id": 1 }"#,
