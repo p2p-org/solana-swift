53c4463ee97dbbd75c27174daefd2067c72131e1
Tran Giang Long <trangianglong4003@gmail.com>
2024-01-23 01:06:20 +0700

    feat: change types

--- Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ParsedInterestBearingConfig.swift
+++ Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ParsedInterestBearingConfig.swift
@@ -4,9 +4,9 @@ public struct ParsedInterestBearingConfig: Codable {
     public static let name: String = "interestBearingConfig"
     
     public let currentRate: Int64
-    public let preUpdateAverageRate: Int64
-    public let initializationTimestamp: Int64
-    public let lastUpdateTimestamp: Int64
+    public let preUpdateAverageRate: UInt64
+    public let initializationTimestamp: UInt64
+    public let lastUpdateTimestamp: UInt64
     public let rateAuthority: PublicKey
 }
 
--- Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ParsedTransferFeeConfig.swift
+++ Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ParsedTransferFeeConfig.swift
@@ -4,12 +4,12 @@ public struct ParsedTransferFeeConfig: Codable {
     public static let name: String = "transferFeeConfig"
 
     public struct TransferFee: Codable {
-        let epoch: Int64
-        let maximumFee: Double
-        let transferFeeBasisPoints: Int64
+        let epoch: UInt64
+        let maximumFee: UInt64
+        let transferFeeBasisPoints: UInt64
     }
 
     public let newerTransferFee, olderTransferFee: TransferFee
     public let transferFeeConfigAuthority, withdrawWithheldAuthority: String
-    public let withheldAmount: Int64
+    public let withheldAmount: UInt64
 }
