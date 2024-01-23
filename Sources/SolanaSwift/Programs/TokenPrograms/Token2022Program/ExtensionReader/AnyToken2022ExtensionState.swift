ad3580ba20523f44c5b9aedfdcaffc961c0240b0
Chung Tran <bigearsenal@gmail.com>
2024-01-23 17:03:41 +0700

    feat: some fixes

---
+++ Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ExtensionReader/AnyToken2022ExtensionState.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+public struct AnyToken2022ExtensionState: BorshCodable, Codable, Equatable, Hashable {
+    // MARK: - Hashable
+
+    public func hash(into hasher: inout Hasher) {
+        type.hash(into: &hasher)
+        state.hash(into: &hasher)
+    }
+
+    // MARK: - Equatable
+
+    public static func == (lhs: AnyToken2022ExtensionState, rhs: AnyToken2022ExtensionState) -> Bool {
+        lhs.type == rhs.type &&
+            lhs.state.jsonString == rhs.state.jsonString
+    }
+
+    // MARK: - Properties
+
+    public let type: Token2022ExtensionType
+    public let state: any Token2022ExtensionState
+
+    // MARK: - Codable
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case state
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(Token2022ExtensionType.self, forKey: .type)
+
+        switch type {
+        case .transferFeeConfig:
+            state = try container.decode(TransferFeeConfigExtensionState.self, forKey: .state)
+        default:
+            state = try container.decode(UnparsedExtensionState.self, forKey: .state)
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+        try container.encode(state, forKey: .state)
+    }
+
+    // MARK: - BorshCodable
+
+    public init(from reader: inout BinaryReader) throws {
+        guard let type = try Token2022ExtensionType(rawValue: UInt16(from: &reader)) else {
+            throw BinaryReaderError.dataMismatch
+        }
+        self.type = type
+        switch type {
+        case .transferFeeConfig:
+            state = try TransferFeeConfigExtensionState(from: &reader)
+        default:
+            state = try UnparsedExtensionState(from: &reader)
+        }
+    }
+
+    public func serialize(to data: inout Data) throws {
+        try type.rawValue.serialize(to: &data)
+        try state.serialize(to: &data)
+    }
+}
--- Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ExtensionReader/Token2022ExtensionType.swift
+++ Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/ExtensionReader/Token2022ExtensionType.swift
@@ -1,72 +1,72 @@
 import Foundation
 
-public enum ExtensionType: UInt16, Codable, Hashable {
+public enum Token2022ExtensionType: UInt16, Codable, Hashable {
     /// Used as padding if the account size would otherwise be 355, same as a
     /// multisig
-    case Uninitialized
+    case uninitialized
     /// Includes transfer fee rate info and accompanying authorities to withdraw
     /// and set the fee
-    case TransferFeeConfig
+    case transferFeeConfig
     /// Includes withheld transfer fees
-    case TransferFeeAmount
+    case transferFeeAmount
     /// Includes an optional mint close authority
-    case MintCloseAuthority
+    case mintCloseAuthority
     /// Auditor configuration for confidential transfers
-    case ConfidentialTransferMint
+    case confidentialTransferMint
     /// State for confidential transfers
-    case ConfidentialTransferAccount
+    case confidentialTransferAccount
     /// Specifies the default Account::state for new Accounts
-    case DefaultAccountState
+    case defaultAccountState
     /// Indicates that the Account owner authority cannot be changed
-    case ImmutableOwner
+    case immutableOwner
     /// Require inbound transfers to have memo
-    case MemoTransfer
-    /// Indicates that the tokens from this mint can't be transfered
-    case NonTransferable
+    case memoTransfer
+    /// Indicates that the tokens from this mint can't be transferred
+    case nonTransferable
     /// Tokens accrue interest over time,
-    case InterestBearingConfig
+    case interestBearingConfig
     /// Locks privileged token operations from happening via CPI
-    case CpiGuard
+    case cpiGuard
     /// Includes an optional permanent delegate
-    case PermanentDelegate
+    case permanentDelegate
     /// Indicates that the tokens in this account belong to a non-transferable
     /// mint
-    case NonTransferableAccount
+    case nonTransferableAccount
     /// Mint requires a CPI to a program implementing the "transfer hook"
     /// interface
-    case TransferHook
+    case transferHook
     /// Indicates that the tokens in this account belong to a mint with a
     /// transfer hook
-    case TransferHookAccount
+    case transferHookAccount
     /// Includes encrypted withheld fees and the encryption public that they are
     /// encrypted under
-    case ConfidentialTransferFeeConfig
+    case confidentialTransferFeeConfig
     /// Includes confidential withheld transfer fees
-    case ConfidentialTransferFeeAmount
+    case confidentialTransferFeeAmount
     /// Mint contains a pointer to another account (or the same account) that
     /// holds metadata
-    case MetadataPointer
+    case metadataPointer
     /// Mint contains token-metadata
-    case TokenMetadata
+    case tokenMetadata
     /// Mint contains a pointer to another account (or the same account) that
     /// holds group configurations
-    case GroupPointer
+    case groupPointer
     /// Mint contains token group configurations
-    case TokenGroup
+    case tokenGroup
     /// Mint contains a pointer to another account (or the same account) that
     /// holds group member configurations
-    case GroupMemberPointer
+    case groupMemberPointer
     /// Mint contains token group member configurations
-    case TokenGroupMember
+    case tokenGroupMember
 
     // MARK: - Test only
 
-//    /// Test variable-length mint extension
-//    case VariableLenMintTest = UInt16.max - 2,
-//    /// Padding extension used to make an account exactly Multisig::LEN, used
-//    /// for testing
-//    case AccountPaddingTest = UInt16.max - 1
-//    /// Padding extension used to make a mint exactly Multisig::LEN, used for
-//    /// testing
-//    case MintPaddingTest = UInt16.max
+    //    /// Test variable-length mint extension
+    //    case variableLenMintTest = UInt16.max - 2,
+    //    /// Padding extension used to make an account exactly Multisig::LEN, used
+    //    /// for testing
+    //    case accountPaddingTest = UInt16.max - 1
+    //    /// Padding extension used to make a mint exactly Multisig::LEN, used for
+    //    /// testing
+    //    case mintPaddingTest = UInt16.max
 }
